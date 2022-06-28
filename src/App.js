f8bca0f07ab078aca2b119f962bf1704944a7747
kristenpewar <kpewar222@gmail.com>
2022-06-28 12:05:36 -0700

    folder creation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './App.css';
+import Home from './pages';
 
 function App() {
   return (
     <div className="App">
-      <h1> Heading test</h1>
+      <Home></Home>
     </div>
   );
 }
 
 export default App;
+
+//this is where new pages can be added, so far there is only a home page for the website
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    <div>
+        <div>Heading Test</div>
+    </div>
+  )
+}
+
+export default Home
+
+//this is the index for the "Home" page, the entire smooth scroll page
