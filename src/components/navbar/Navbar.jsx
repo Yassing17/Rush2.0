66f5f4d0c1173b44ce2b6d3d9d47ff0aaf19c035
Yassin Guitouni <Yassin.guitouni@telus.net>
2022-06-28 17:32:46 -0700

    navbar commits

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { FaBars, FaFacebook, FaTimes, FaInstagram } from 'react-icons/fa'
+import { GiCarWheel } from 'react-icons/gi'
+import './NavbarStyles.css'
+import { Link } from 'react-scroll'
+
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const [slide, setSlide] = useState(false)
+
+    const handleNav = () => {
+        setNav(!nav)
+        setSlide(!slide)
+    }
+
+    const handleClose = () => {
+        setNav(!nav)
+    }
+
+    return (
+        <div className='navbar'>
+            <div className="container">
+                <div className={slide ? 'logo slide-right' : 'logo'}>
+                    <h3>RUSH</h3>
+                </div>
+
+                {/*lots of code from old website to keep scrolling cpapbilities */}
+                <ul className={nav ? 'nav-menu active' : 'nav-menu'}>
+                    <li><a href="/"><Link onClick={handleClose} activeClass="active" to="power" spy={true} smooth={true} duration={500}>Dashboards</Link></a></li>
+                    <li><a href="/"><Link onClick={handleClose} activeClass="active" to="speed" spy={true} smooth={true} duration={500}>Events</Link></a></li>
+
+                    <div className='mobile-menu'>
+                        <button>About Us</button>
+                        <button>Contact</button>
+                        <div className="social-icons">
+                            <FaFacebook className='icon' />
+                            <FaInstagram className='icon' />
+                            <GiCarWheel className='icon' />
+                        </div>
+                    </div>
+
+                </ul>
+
+                <ul className='nav-menu hide'>
+                    <li><a href="/">Shop</a></li>
+                    <li><a href="/">Account</a></li>
+                </ul>
+
+                <div className="hamburger" onClick={handleNav} >
+                    {nav ? (<FaTimes size={20} style={{ color: '#ffffff' }} />) : (<FaBars style={{ color: '#ffffff' }} size={20} />)}
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
