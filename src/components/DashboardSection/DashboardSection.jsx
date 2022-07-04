c532f0a96bf2a6c6e2b1a98961091e087662ae1d
Yassin Guitouni <Yassin.guitouni@telus.net>
2022-07-04 15:08:35 -0700

    revisit for sure

--- src/components/DashboardSection/DashboardSection.jsx
+++ src/components/DashboardSection/DashboardSection.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './DashboardSectionStyles.css'
 
 const DashboardSection = () => {
   return (
-    <div>DashboardSection</div>
+    <div name='dashpage' className='dashpage'>
+      <div className="dpcontainer">
+        <div className='top'>
+          <div className='dpcontainer2'>
+             <h1>Dashbaords</h1>
+          </div>
+        </div>  
+        <div className="bottom">
+            <button className="btn btn-dark">Learn</button>
+            <button className="btn btn-dark">Act</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
